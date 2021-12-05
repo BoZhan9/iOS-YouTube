a12b95db68ecd5b9bd5258f0a7dc600575793253
Brian Zhang <bz@Brians-MacBook-Air.local>
2021-12-04 20:09:50 -0800

    Add CacheManager

---
+++ iOS-YouTube/Helpers/CacheManager.swift
@@ -0,0 +1,21 @@
+//
+//  CacheManager.swift
+//  iOS-YouTube
+//
+//  Created by Brian Zhang on 12/4/21.
+//
+
+import Foundation
+
+class CacheManager {
+    
+    static var cache = [String:Data]()
+    
+    static func setVideoCache(_ url:String, _ data:Data?) {
+        // Store the image data and use the url as the key
+        cache[url] = data
+    }
+    static func getVideoCache(_ url:String) -> Data? {
+        return cache[url]
+    }
+}
--- iOS-YouTube/Views/VideoTableViewCell.swift
+++ iOS-YouTube/Views/VideoTableViewCell.swift
@@ -45,12 +45,24 @@ class VideoTableViewCell: UITableViewCell {
             return
         }
         
+        if let cacheData = CacheManager.getVideoCache(self.video!.thumbnail) {
+            self.thumbnailImageView.image = UIImage(data: cacheData)
+            return
+        }
+        
         let url = URL(string: self.video!.thumbnail)
         
         let session = URLSession.shared
         
         let dataTask = session.dataTask(with: url!) { data, reponse, error in
             if error == nil && data != nil {
+                CacheManager.setVideoCache(url!.absoluteString, data)
+                
+                if url?.absoluteString != self.video?.thumbnail {
+                    return
+                }
+                
+                
                 if url?.absoluteString != self.video?.thumbnail {
                     return
                 }

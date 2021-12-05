c507270d5b213905205edcae42e7a7fd0364cf86
Brian Zhang <bz@Brians-MacBook-Air.local>
2021-12-04 18:25:15 -0800

    Modify some files

---
+++ iOS-YouTube/ConstantsTemplete.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  iOS-YouTube
+//
+//  Created by Brian Zhang on 12/4/21.
+//
+
+import Foundation
+
+/*
+struct Constants {
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
+*/
+
+
